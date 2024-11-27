599981c8cc308fe847b7b8462e185754ab808363
amittanwar777 <41777609+amittanwar777@users.noreply.github.com>
2024-11-27 17:16:44 +0530

    Update Startbash.sh

--- Startbash.sh
+++ Startbash.sh
@@ -1,4 +1,10 @@
-autoload -Uz vcs_info
-precmd() { vcs_info }
-PS1='%n@%m:%~${vcs_info_msg_0_}%# '
-zstyle ':vcs_info:git:*' formats ' (%b)'
+# Add the following to your .bashrc file:
+
+PS1="\u@\h:\w \$(parse_git_branch)\$ "
+
+parse_git_branch() {
+  git branch 2> /dev/null | grep -v "^*" | awk '{print "(" "$NF" ")"}'
+}
+
+# Source the changes:
+source ~/.bashrc
